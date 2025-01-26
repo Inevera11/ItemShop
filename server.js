58f1a4215c8c538c8c4870b980a6e10bace92ba9
Inevera11 <zuzannabozek2002@gmail.com>
2025-01-26 11:55:19 +0100

    add ejs

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/error' });
 });
 
 app.listen(3000);
